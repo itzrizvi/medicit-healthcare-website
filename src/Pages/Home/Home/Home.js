47e11afe4e9fdbe72a3b7af7b63e7809c07163f6
itzrizvi <shahriar.me.work@gmail.com>
2021-10-20 14:53:00 +0600

    Surprise Body Section Done

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Banner from '../Banner/Banner';
 import IntensiveUnit from '../IntensiveUnit/IntensiveUnit';
 import Services from '../Services/Services';
+import SurpriseBody from '../SurpriseBody/SurpriseBody';
 
 // HOME CONTAINER COMPONENT
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <SurpriseBody></SurpriseBody>
             <Services></Services>
             <IntensiveUnit></IntensiveUnit>
         </div>
---
+++ src/Pages/Home/SurpriseBody/SurpriseBody.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './SurpriseBody.css';
+import surPriseImg from '../../../Images/surpriseyourbody.png';
+import ratingShape from '../../../Images/ratingShape.png';
+import { Col, Container, Row } from 'react-bootstrap';
+
+const SurpriseBody = () => {
+    return (
+        <>
+            <div className="surprise-body-section">
+                <Container>
+                    <Row>
+                        <Col md={6} sm={6} xs={12}>
+                            <div className="surpise-body-img">
+                                <img src={surPriseImg} className="img-fluid" alt="SURPRISEBODYIMG" />
+                            </div>
+                        </Col>
+                        <Col md={6} sm={6} xs={12}>
+                            <div className="surprise-body-details text-start">
+                                <div className="surprise-heading">
+                                    <h2><span style={{ color: ' #7dd6f6' }}>Surprise Your Body</span> With Extra Care...</h2>
+                                    <p>Rapidiously evisculate user-centric functionalities for highly efficient nterfaces.
+                                        Competently leverage other's scalable technology before synergistic manufactured products.</p>
+                                </div>
+                                <div className="rating-area">
+                                    <img src={ratingShape} alt="ratingshape" />
+                                    <div className="rating-text d-flex">
+                                        <h2>4.8</h2>
+                                        <p>Medicit Overall Rating Based on 4957 Reviews.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        </>
+    );
+};
+
+export default SurpriseBody;
